aff75ec2fa791852f11909ad488ec1e0328fb5d6
Jakub Nitkiewicz <01158207@pw.edu.pl>
2023-05-23 20:28:08 +0200

    Fix DeletePlantFromMicrocontroller

--- DeletePlantFromMicrocontroller/index.js
+++ DeletePlantFromMicrocontroller/index.js
@@ -1,10 +1,10 @@
 module.exports = async function (context, req) {
-    let plantsBySenors = context.bindings.mcIn[0].plantsBySenors;
-    const sensors = plantsBySenors.map((plant) => Object.keys(plant)[0]);
-    const index = sensors.indexOf(req.query.sensor);
-    if (index > -1) {
-        plantsBySenors.splice(index, 1);
-    }
-    context.bindings.mcOut = context.bindings.mcIn;
-    context.bindings.mcOut[0].plantsBySenors = plantsBySenors;
-}
+  let plantsBySensors = context.bindings.mcIn[0].plantsBySensors;
+  const sensors = plantsBySensors.map((plant) => Object.keys(plant)[0]);
+  const index = sensors.indexOf(req.query.sensor);
+  if (index > -1) {
+    plantsBySensors.splice(index, 1);
+  }
+  context.bindings.mcOut = context.bindings.mcIn;
+  context.bindings.mcOut[0].plantsBySensors = plantsBySensors;
+};

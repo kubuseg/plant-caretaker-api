356bb278fdbc3e1b141338b866c95adc89779660
Jakub Nitkiewicz <01158207@pw.edu.pl>
2023-05-23 20:13:05 +0200

    Fix in AddPlantToMicrocontroller

--- AddPlantToMicrocontroller/index.js
+++ AddPlantToMicrocontroller/index.js
@@ -1,11 +1,11 @@
-
 module.exports = async function (context, req) {
-    context.bindings.mcOut = context.bindings.mcIn;
-    const userPlants = context.bindings.users[0].plants;
-    const userPlantsUUID = userPlants.map((plant) => plant.uuid);
-    const index = userPlantsUUID.indexOf(req.query.plantUUID);
-    if (index > -1) {
-        context.bindings.mcOut[0].plantsBySenors.push({[req.query.sensorId]: userPlants[index]});
-    }    
-}
-    
+  context.bindings.mcOut = context.bindings.mcIn;
+  const userPlants = context.bindings.users[0].plants;
+  const userPlantsUUID = userPlants.map((plant) => plant.uuid);
+  const index = userPlantsUUID.indexOf(req.body.plantUUID);
+  if (index > -1) {
+    context.bindings.mcOut[0].plantsBySensors.push({
+      [req.body.sensorId]: userPlants[index],
+    });
+  }
+};

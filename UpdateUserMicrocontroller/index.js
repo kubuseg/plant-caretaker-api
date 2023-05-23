55d3d718fc705a856a8a3ca7a0f82839ffa81f8a
Jakub Nitkiewicz <01158207@pw.edu.pl>
2023-05-23 19:35:26 +0200

    Change UpdateUserPlant and UpdateUserMicrocontroller

--- UpdateUserMicrocontroller/index.js
+++ UpdateUserMicrocontroller/index.js
@@ -1,4 +1,4 @@
 module.exports = async function (context, req) {
   context.bindings.usersOut = context.bindings.usersIn;
-  context.bindings.usersOut[0].mcId = req.query.mcId;
+  context.bindings.usersOut[0].mcId = req.body.mcId;
 };
--- UpdateUserPlant/index.js
+++ UpdateUserPlant/index.js
@@ -2,7 +2,7 @@ module.exports = async function (context, req) {
   context.bindings.usersOut = context.bindings.usersIn[0];
   const plants = context.bindings.usersIn[0].plants;
   const plantsUUID = plants.map((plant) => plant.uuid);
-  const index = plantsUUID.indexOf(req.query.uuid);
+  const index = plantsUUID.indexOf(req.body.uuid);
   if (index > -1) {
     plants[index] = req.body.plant;
   }

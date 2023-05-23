890fcc46e28322d79287832517ae08e095194696
Jakub Nitkiewicz <01158207@pw.edu.pl>
2023-05-23 22:24:28 +0200

    Changed IncertMeasuremetnsTrigger so it works with body instead of query

--- InsertMeasurmentsTrigger/index.js
+++ InsertMeasurmentsTrigger/index.js
@@ -1,11 +1,11 @@
 module.exports = async function (context, req) {
-  const humidities = JSON.parse(req.query.humidities);
+  const humidities = JSON.parse(req.body.humidities);
   const measurementsOut = [];
   const date = new Date().toISOString();
   for (i = 0; i < humidities.length; ++i) {
     measurementsOut.push({
       dateTime: date,
-      mcId: req.query.mcId,
+      mcId: req.body.mcId,
       sensorId: Object.keys(humidities[i])[0],
       humidity: Object.values(humidities[i])[0].humidity,
     });

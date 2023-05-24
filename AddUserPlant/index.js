e0fdfc29873c24a932aefdb9cfda59e0e9ab9896
Jakub Nitkiewicz <01158207@pw.edu.pl>
2023-05-24 12:03:34 +0200

    Add uuid to response in AddUserPlant

--- AddUserPlant/index.js
+++ AddUserPlant/index.js
@@ -1,9 +1,12 @@
 const crypto = require("crypto");
 
 module.exports = async function (context) {
-    context.bindings.usersOut = context.bindings.usersIn;
-    const plant = context.bindings.plant[0];
-    plant.uuid = crypto.randomUUID();
-    plant.flowerpotSize = "SMALL";
-    context.bindings.usersOut[0].plants.push(plant)
-}
+  context.bindings.usersOut = context.bindings.usersIn;
+  const plant = context.bindings.plant[0];
+  plant.uuid = crypto.randomUUID();
+  plant.flowerpotSize = "SMALL";
+  context.bindings.usersOut[0].plants.push(plant);
+  context.res = {
+    body: { uuid: plant.uuid },
+  };
+};
